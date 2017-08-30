7e6477804e94c1cbceb1e4596ddf23133edcfe23
Paweł Jan Czochański <pawel.czochanski@symmetra.pl>
2017-08-30 13:39:15 +0200

    Add clean command
    Clean command removes unused keys from the keyring.

--- pigeon.py
+++ pigeon.py
@@ -20,7 +20,7 @@ def list_missing_keys():
 
 def check_cmd():
     for user, fingerprint in list_missing_keys():
-        print('No key available for user {} (fingerprint: {})'
+        print('No key available for user {} (fingerprint: {}).'
               .format(user, fingerprint), file=sys.stderr)
 
 
@@ -28,6 +28,14 @@ def import_key_cmd(path):
     with open(path, 'r') as keyfile:
         GPG.import_keys(keyfile.read())
 
+
+def clean_cmd():
+    available_keys = set(map(lambda k: k['fingerprint'], GPG.list_keys()))
+    required_keys = set(map(lambda k: k['fingerprint'], config.USERS.values()))
+    for fingerprint in available_keys - required_keys:
+        print('Deleting key: {}.'.format(fingerprint), file=sys.stderr)
+        GPG.delete_keys(fingerprint)
+    print('Key database clean.', file=sys.stderr)
 
 def run_cmd():
     pass  # TODO run pigeon
@@ -49,18 +57,22 @@ def main():
                                    help='path to the keyfile to import')
     import_key_parser.set_defaults(command='importkey')
 
+    clean_parser = subparsers.add_parser('clean', help='clean help')
+    clean_parser.set_defaults(command='clean')
+
     args = parser.parse_args()
 
     if args.command == 'check':
         check_cmd()
     elif args.command == 'importkey':
         import_key_cmd(args.path)
+    elif args.command == 'clean':
+        clean_cmd()
     elif args.command == 'run':
         if list_missing_keys():
             print('Some keys seem to be missing, run the \'check\' command.',
                   file=sys.stderr)
         run_cmd()
-
 
 if __name__ == "__main__":
     main()
