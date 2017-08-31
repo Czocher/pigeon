cfcf67c50cad404d2f04de021a32b51fbf917947
Paweł Jan Czochański <pawel.czochanski@symmetra.pl>
2017-08-31 12:50:33 +0200

    Refactor imports

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
+from aiosmtpd.controller import Controller
+
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.encoders import encode_7or8bit
 
 import asyncio
-
-import email.mime.application
-import email.mime.multipart
-import email.mime.text
-import email.encoders
-
+import sys
 import gnupg
-
 import config
 
 
@@ -19,22 +19,20 @@ gpg = gnupg.GPG(homedir=config.GPGHOME)
 def pgp_mime(message, recepients):
     content = gpg.encrypt(message.as_string(), recepients)
 
-    encrypted = email.mime.application.MIMEApplication(
+    encrypted = MIMEApplication(
             _data=str(content),
             _subtype='octet-stream; name="encrypted.asc"',
-            _encoder=email.encoders.encode_7or8bit)
+            _encoder=encode_7or8bit)
     encrypted['Content-Description'] = 'OpenPGP encrypted message'
     encrypted.set_charset('us-ascii')
 
-    control = email.mime.application.MIMEApplication(
+    control = MIMEApplication(
             _data='Version: 1\n',
             _subtype='pgp-encrypted',
-            _encoder=email.encoders.encode_7or8bit)
+            _encoder=encode_7or8bit)
     control.set_charset('us-ascii')
 
-    encmsg = email.mime.multipart.MIMEMultipart(
-            'encrypted',
-            protocol='application/pgp-encrypted')
+    encmsg = MIMEMultipart('encrypted', protocol='application/pgp-encrypted')
     encmsg.attach(control)
     encmsg.attach(encrypted)
     encmsg['Content-Disposition'] = 'inline'
@@ -52,7 +50,7 @@ class PigeonHandler:
         return '250 OK'
 
     async def handle_DATA(self, server, session, envelope):
-        message = email.mime.text.MIMEText(
+        message = MIMEText(
                 _text=envelope.content.decode('utf-8', errors='replace'))
         # TODO can len(rcpt_tos) > 1?
         fingerprints = map(
@@ -63,10 +61,8 @@ class PigeonHandler:
 
 
 def main():
-    # TODO handle address and port
-    from aiosmtpd.controller import Controller
-    import asyncio
-    controller = Controller(PigeonHandler(), hostname='localhost', port=8025)
+    address, port = sys.argv[1:3]
+    controller = Controller(PigeonHandler(), hostname=address, port=port)
     controller.start()
     loop = asyncio.get_event_loop()
     loop.run_forever()
